5d6e712d91e787e111ef047815159fcd9d6c6004
wangmengtao <wangmengtao@baidu.com>
2020-01-14 02:17:50 +0800

    add broker unit test, add will-retain unit test

--- broker_test.go
+++ broker_test.go
@@ -14,24 +14,38 @@ const (
 	cfg = "./example/etc/baetyl/service.yml"
 )
 
-func TestSessionMqttConnect(t *testing.T) {
+func TestSessionMqttConnectTCP(t *testing.T) {
 	var config Config
 	utils.LoadYAML(cfg, &config)
 	b, err := NewBroker(config)
 	assert.NoError(t, err)
-	assert.True(t, b.transport.Alive())
 	defer b.Close()
 	time.Sleep(time.Millisecond * 100)
 
 	// for tcp connection
 	dailer := mqtt.NewDialer(nil, time.Duration(0))
 	pkt := mqtt.NewConnect()
-	pkt.ClientID = b.transport.GetServers()[0].Addr().String()
-	conn, err := dailer.Dial(getURL(b.transport.GetServers()[0], "tcp"))
+	pkt.ClientID = "tcp-1"
+	pkt.Username = "test"
+	pkt.Password = "hahaha"
+	tcpConn, err := dailer.Dial(getURL(b.transport.GetServers()[0], "tcp"))
 	assert.NoError(t, err)
-	err = conn.Send(pkt, true)
+	defer tcpConn.Close()
+	err = tcpConn.Send(pkt, true)
 	assert.NoError(t, err)
-	conn.Close()
+	pktRes, err := tcpConn.Receive()
+	assert.NoError(t, err)
+	pktString := pktRes.String()
+	assert.Equal(t, "<Connack SessionPresent=false ReturnCode=0>", pktString)
+}
+
+func TestSessionMqttConnectSSL(t *testing.T) {
+	var config Config
+	utils.LoadYAML(cfg, &config)
+	b, err := NewBroker(config)
+	assert.NoError(t, err)
+	defer b.Close()
+	time.Sleep(time.Millisecond * 100)
 
 	// for ssl connection
 	ctc, err := utils.NewTLSConfigClient(utils.Certificate{
@@ -41,37 +55,76 @@ func TestSessionMqttConnect(t *testing.T) {
 		InsecureSkipVerify: true,
 	})
 	assert.NoError(t, err)
-	dailer = mqtt.NewDialer(ctc, time.Duration(0))
-	pkt.ClientID = b.transport.GetServers()[1].Addr().String()
-	conn, err = dailer.Dial(getURL(b.transport.GetServers()[1], "ssl"))
+	dailer := mqtt.NewDialer(ctc, time.Duration(0))
+	pkt := mqtt.NewConnect()
+	pkt.ClientID = "ssl-1"
+	pkt.Username = ""
+	pkt.Password = ""
+	sslConn, err := dailer.Dial(getURL(b.transport.GetServers()[1], "ssl"))
 	assert.NoError(t, err)
-	err = conn.Send(pkt, false)
+	defer sslConn.Close()
+	err = sslConn.Send(pkt, true)
 	assert.NoError(t, err)
-	conn.Close()
+	pktRes, err := sslConn.Receive()
+	assert.NoError(t, err)
+	pktString := pktRes.String()
+	assert.Equal(t, "<Connack SessionPresent=false ReturnCode=0>", pktString)
+}
+
+func TestSessionMqttConnectWebsocket(t *testing.T) {
+	var config Config
+	utils.LoadYAML(cfg, &config)
+	b, err := NewBroker(config)
+	assert.NoError(t, err)
+	defer b.Close()
+	time.Sleep(time.Millisecond * 100)
 
 	// for websocket connection
-	dailer = mqtt.NewDialer(nil, time.Duration(0))
-	pkt.ClientID = b.transport.GetServers()[2].Addr().String()
-	conn, err = dailer.Dial(getURL(b.transport.GetServers()[2], "ws") + "/mqtt")
+	dailer := mqtt.NewDialer(nil, time.Duration(0))
+	pkt := mqtt.NewConnect()
+	pkt.ClientID = "websocket-1"
+	pkt.Username = "test"
+	pkt.Password = "hahaha"
+	wsConn, err := dailer.Dial(getURL(b.transport.GetServers()[2], "ws") + "/mqtt")
 	assert.NoError(t, err)
-	err = conn.Send(pkt, true)
+	defer wsConn.Close()
+	err = wsConn.Send(pkt, true)
 	assert.NoError(t, err)
-	conn.Close()
+	pktRes, err := wsConn.Receive()
+	assert.NoError(t, err)
+	pktString := pktRes.String()
+	assert.Equal(t, "<Connack SessionPresent=false ReturnCode=0>", pktString)
+}
+
+func TestSessionMqttConnectWebscoketSSL(t *testing.T) {
+	var config Config
+	utils.LoadYAML(cfg, &config)
+	b, err := NewBroker(config)
+	assert.NoError(t, err)
+	defer b.Close()
+	time.Sleep(time.Millisecond * 100)
 
 	// for wss(websocket + ssl) connection
-	ctc, err = utils.NewTLSConfigClient(utils.Certificate{
+	ctc, err := utils.NewTLSConfigClient(utils.Certificate{
 		CA:                 "./example/testcert/ca.pem",
 		Cert:               "./example/testcert/client.pem",
 		Key:                "./example/testcert/client.key",
 		InsecureSkipVerify: true,
 	})
 	assert.NoError(t, err)
-	dailer = mqtt.NewDialer(ctc, time.Duration(0))
-	pkt.ClientID = b.transport.GetServers()[3].Addr().String()
-	conn, err = dailer.Dial(getURL(b.transport.GetServers()[3], "wss") + "/mqtt")
+	dailer := mqtt.NewDialer(ctc, time.Duration(0))
+	pkt := mqtt.NewConnect()
+	pkt.ClientID = "wss-1"
+	pkt.Username = ""
+	pkt.Password = ""
+	wssConn, err := dailer.Dial(getURL(b.transport.GetServers()[3], "wss") + "/mqtt")
 	assert.NoError(t, err)
-	defer conn.Close()
-	err = conn.Send(pkt, false)
+	defer wssConn.Close()
+	err = wssConn.Send(pkt, true)
+	pktRes, err := wssConn.Receive()
+	assert.NoError(t, err)
+	pktString := pktRes.String()
+	assert.Equal(t, "<Connack SessionPresent=false ReturnCode=0>", pktString)
 	assert.NoError(t, err)
 }
 
