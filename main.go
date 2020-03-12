5bbd9d55b3794adcc4c07d9b677585e91486e3d8
ludanfeng <ludanfeng@baidu.com>
2020-03-12 20:23:47 +0800

    update baetyl go sdk

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	// _ "net/http/pprof"
 
 	"github.com/baetyl/baetyl-go/context"
-	"github.com/baetyl/baetyl-go/utils"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -28,14 +27,8 @@ func main() {
 	// defer trace.Stop()
 
 	context.Run(func(ctx context.Context) error {
-		var err error
 		var cfg Config
-		// TODO: ctx.LoadCustomConfig(&cfg, "")
-		if utils.FileExists("etc/baetyl/service.yml") {
-			err = utils.LoadYAML("etc/baetyl/service.yml", &cfg)
-		} else {
-			err = utils.UnmarshalYAML(nil, &cfg)
-		}
+		err := ctx.LoadCustomConfig(&cfg)
 		if err != nil {
 			return err
 		}
