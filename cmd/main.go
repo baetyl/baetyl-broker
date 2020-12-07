59c53deb4f3e8324da7bcba8b6e58c6a63a8fcb2
chensheng06 <1145461707@qq.com>
2020-12-07 15:23:59 +0800

    ready to modify get

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,25 @@
 package main
 
 import (
-	// "net/http"
-	// _ "net/http/pprof"
+	"net/http"
+	_ "net/http/pprof"
 
 	"github.com/baetyl/baetyl-go/v2/context"
 
 	"github.com/baetyl/baetyl-broker/v2/broker"
-	"github.com/baetyl/baetyl-broker/v2/listener"
-
 	_ "github.com/baetyl/baetyl-broker/v2/store/pebble"
 )
 
 func main() {
-	// // go tool pprof http://localhost:6060/debug/pprof/profile
-	// go func() {
-	// 	panic(http.ListenAndServe(":6060", nil))
-	// }()
-
-	// f, err := os.Create("trace.out")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer f.Close()
-
-	// err = trace.Start(f)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer trace.Stop()
+	// go tool pprof http://localhost:6060/debug/pprof/profile
+	go func() {
+		panic(http.ListenAndServe(":8005", nil))
+	}()
 
 	context.Run(func(ctx context.Context) error {
-		if err := ctx.CheckSystemCert(); err != nil {
-			return err
-		}
+		//if err := ctx.CheckSystemCert(); err != nil {
+		//	return err
+		//}
 
 		var cfg broker.Config
 		err := ctx.LoadCustomConfig(&cfg)
@@ -41,12 +27,12 @@ func main() {
 			return err
 		}
 
-		cert := ctx.SystemConfig().Certificate
-		cfg.Listeners = append(cfg.Listeners, listener.Listener{
-			Address:     "ssl://0.0.0.0:" + ctx.BrokerPort(),
-			Anonymous:   true,
-			Certificate: cert,
-		})
+		//cert := ctx.SystemConfig().Certificate
+		//cfg.Listeners = append(cfg.Listeners, listener.Listener{
+		//	Address:     "ssl://0.0.0.0:" + ctx.BrokerPort(),
+		//	Anonymous:   true,
+		//	Certificate: cert,
+		//})
 
 		b, err := broker.NewBroker(cfg)
 		if err != nil {
